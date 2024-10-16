f34bfd8af913e06ebc66603717e43cc3c1d4fb2e
Subin Cho, Ph.D. <128745345+sbeeniNY@users.noreply.github.com>
2024-10-16 16:06:38 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Blog-1211817a34e580aeaa1be426d6b3d732?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Projects wiki collection',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Subin Cho',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
